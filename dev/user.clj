d1cd3d790f1f6fc81e9ad138d21b16ec033155a4
clifford goldberg <clifford.goldberg@gmail.com>
2015-01-12 20:22:50 +0200

    compojure and http-kit working

--- dev/user.clj
+++ dev/user.clj
@@ -1,6 +1,17 @@
 (ns user
   (:require [leiningen.core.main :as lein]
             [clojure.java.io :as io]
+
+            [ring.util.http-response :refer [ok content-type header] :as resp]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.nested-params :refer [wrap-nested-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.format :refer [wrap-restful-format]]
+            [bidi.ring :as br]
+            [ring.util.response :refer (file-response url-response response)]
+            [org.httpkit.server :as httpkit]
+
+            ;;[bidi.ring :refer (make-handler)]
             [compojure.core :refer [GET defroutes]]
             [compojure.route :refer [resources]]
             [compojure.handler :refer [api]]
@@ -8,6 +19,27 @@
             [environ.core :refer [env]]
             [ring.adapter.jetty :refer [run-jetty]]))
 
+;; trying to use juxt/bidi
+;; (def routes
+;;   ["/" {"index.html" ::home-page-handler
+;;         "" (br/resources-maybe {:prefix "resources/public"})}])
+
+;; (def handlers
+;;   {::home-page-handler (fn [req]
+;;                          (->
+;;                           (io/resource "index.html")
+;;                           (slurp)
+;;                           (ok))
+;;                          ;;(response "Hello world!")
+;;                          )})
+
+;; ;; (def http-handler (br/make-handler routes handlers))
+;; (def http-handler (br/make-handler routes
+;;                   (some-fn handlers
+;;                            #(when (fn? %) %))))
+
+
+;; using compojure
 (defroutes routes
   (resources "/")
   (GET "/*" req (io/resource "index.html")))
@@ -18,8 +50,11 @@
   (defonce ^:private server
     (let [port (Integer. (or port (env :port) 10555))]
       (print "Starting web server on port" port ".\n")
-      (run-jetty http-handler {:port port
-                               :join? false})))
+      (httpkit/run-server (var http-handler) {:port port, :join? false})
+
+      ;; (run-jetty http-handler {:port port
+      ;;                          :join? false})
+      ))
   server)
 
 (defn start-figwheel []
--- project.clj
+++ project.clj
@@ -4,15 +4,22 @@
   :source-paths ["src" "dev"]
 
   :dependencies [
-    [org.clojure/clojure "1.7.0-alpha4"]
+    [org.clojure/clojure "1.7.0-alpha5"]
     [org.clojure/clojurescript "0.0-2511"]
     [datascript "0.7.1"]
     [sablono "0.2.22"]
 ;;    [com.facebook/react "0.11.2"]
     [com.cognitect/transit-cljs "0.8.194"]
+
     [ring "1.3.1"]
+;;    [ring/ring-core "1.3.0"]
+    [metosin/ring-http-response "0.4.0"]
+    [ring-middleware-format "0.3.2" :exclusions [ring]]
+    [http-kit "2.1.18"]
+
     [compojure "1.2.0"]
-    [figwheel "0.2.0-SNAPSHOT"]
+    [bidi "1.15.0"]
+    [figwheel "0.2.1-SNAPSHOT"]
     [environ "1.0.0"]
     [leiningen "2.5.0"]
     [rum "0.1.0"]
@@ -21,10 +28,10 @@
   :min-lein-version "2.5.0"
 
   :plugins [
-    [cider/cider-nrepl "0.8.0-SNAPSHOT"]
-            [lein-cljsbuild "1.0.3"]
+    [cider/cider-nrepl "0.8.1"]
+            [lein-cljsbuild "1.0.4"]
             [lein-environ "1.0.0"]
-            [lein-figwheel "0.2.0-SNAPSHOT"]
+            [lein-figwheel "0.2.1-SNAPSHOT"]
   ]
 
   :figwheel {:http-server-root "public"
